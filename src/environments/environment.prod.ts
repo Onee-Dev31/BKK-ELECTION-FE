bd78a9ead09c171dcdd9ae5699390e0d8745ec04
flukelnw1142 <73615503+flukelnw1142@users.noreply.github.com>
2026-04-22 11:12:18 +0700

    fix oneeclick

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,6 @@ export const environment = {
         summary: 'https://bkk2022static.tillitsdone.com/prodsummary.json',
         districts: 'https://bkk2022static.tillitsdone.com/prodsummaryByDistrict.json?d=Tu-07-03:07'
     },
-    api_url: 'https://electionbkk.one31.net/api',
+    api_url: 'https://electionbkk..oneeclick.co/api',
     name: 'production environment',
 };
