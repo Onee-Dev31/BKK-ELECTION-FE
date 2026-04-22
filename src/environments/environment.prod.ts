c5ee2c1f230146ee0b577208fdb081f908cbf97b
flukelnw1142 <73615503+flukelnw1142@users.noreply.github.com>
2026-04-22 12:20:02 +0700

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,6 @@ export const environment = {
         summary: 'https://bkk2022static.tillitsdone.com/prodsummary.json',
         districts: 'https://bkk2022static.tillitsdone.com/prodsummaryByDistrict.json?d=Tu-07-03:07'
     },
-    api_url: 'https://electionbkk-api.onehd.net:8000/api',
+    api_url: 'https://electionbkk-api.oneeclick.co:8000/api',
     name: 'production environment',
 };
